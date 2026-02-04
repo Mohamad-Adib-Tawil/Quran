9f4a94c334018609c143996a49a1fde2189432d4
Mohamad Adib Tawil <mat.07.mat.70.mat@gmail.com>
2026-02-04 12:03:58 +0300

    ui(search): use SVG search icon from AppAssets

--- lib/features/home/presentation/widgets/home_search_field.dart
+++ lib/features/home/presentation/widgets/home_search_field.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/core/theme/design_tokens.dart';
 import 'package:quran_app/core/theme/figma_palette.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quran_app/core/assets/app_assets.dart';
 
 class HomeSearchField extends StatelessWidget {
   final ValueChanged<String>? onChanged;
@@ -11,7 +13,15 @@ class HomeSearchField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.search),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SvgPicture.asset(
+            AppAssets.icSearch,
+            width: 20,
+            height: 20,
+            colorFilter: const ColorFilter.mode(FigmaPalette.primary, BlendMode.srcIn),
+          ),
+        ),
         hintText: hintText ?? 'ابحث عن سورة',
         filled: true,
         fillColor: FigmaPalette.surfaceMuted,
